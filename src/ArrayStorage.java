0485f177c6c7e5dda061bb6ab4ebb6a253c38f0d
Vasiliy Golban <vgolban@accesssoftek.com>
2019-03-13 11:13:07 +0200

    HW1. Implemented methods for array storage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,56 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        };
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString() == uuid){ return storage[i]; }
+        };
         return null;
     }
 
     void delete(String uuid) {
+        boolean removed = false;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString() == uuid){
+                storage[i] = null;
+                removed = true;
+
+                size--;
+            }
+
+            if (removed){
+                storage[i] = storage[i+1];
+            }
+        };
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResume = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            allResume[i]=storage[i];
+        };
+        return allResume;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
