9e2bff7991e1afc5bb2d91cfbcbe6eba95f44927
Vasiliy Golban <vgolban@accesssoftek.com>
2019-03-31 22:08:13 +0300

    HW4. Call toString() via reflection

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,16 +3,17 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
-        System.out.println(r);
+
+        System.out.println("Reflection toString: " + r.getClass().getDeclaredMethod("toString").invoke(r));
     }
 }
