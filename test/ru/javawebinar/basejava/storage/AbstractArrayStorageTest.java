726e44f5a52664253ca27af89ad41160b8e2fbfa
Vasiliy Golban <vgolban@accesssoftek.com>
2019-04-01 22:23:22 +0300

    HW4. Updated test methods, created objects for testing as fields

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -9,8 +9,7 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class AbstractArrayStorageTest {
     private Storage storage;
@@ -18,17 +17,20 @@ public class AbstractArrayStorageTest {
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
 
-    public AbstractArrayStorageTest(Storage arrayStorage) {
+    AbstractArrayStorageTest(Storage arrayStorage) {
         storage = arrayStorage;
     }
 
     @Before
     public void setUp() throws Exception {
         storage.clear();
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_2));
-        storage.save(new Resume(UUID_3));
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
     }
 
     @Test
@@ -44,31 +46,40 @@ public class AbstractArrayStorageTest {
 
     @Test
     public void get() {
-        assertEquals(UUID_2, storage.get(UUID_2).getUuid());
+        assertEquals(RESUME_2, storage.get(UUID_2));
     }
 
     @Test
     public void update() {
-        storage.update(storage.get(UUID_1));
-        assertEquals(storage.get(UUID_1), storage.get(storage.get(UUID_1).getUuid()));
+        Resume updateResume = new Resume(UUID_1);
+        storage.update(updateResume);
+        assertEquals(updateResume, storage.get(UUID_1));
     }
 
     @Test
     public void save() {
-        Resume resumeSaved = new Resume();
-        storage.save(resumeSaved);
-        assertEquals(resumeSaved, storage.get(resumeSaved.getUuid()));
+        Resume resumeSave = new Resume("resumeSave");
+        storage.save(resumeSave);
+        assertEquals(resumeSave, storage.get(resumeSave.getUuid()));
+        assertEquals(4, storage.size());
     }
 
-    @Test
-    public void delete() {
-        storage.delete(UUID_2);
-        assertEquals(2, storage.size());
+    @Test(expected = NotExistStorageException.class)
+    public void delete() throws Exception {
+        try {
+            storage.delete(UUID_2);
+            assertEquals(2, storage.size());
+        } catch (StorageException e) {
+            fail("Failed to deleted element");
+        }
+        storage.get(UUID_2);
     }
 
     @Test
     public void getAll() {
+        Resume[] expected =  new Resume[]{RESUME_1,RESUME_2,RESUME_3};
         assertEquals(storage.size(), storage.getAll().length);
+        assertArrayEquals(expected, storage.getAll());
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -78,11 +89,11 @@ public class AbstractArrayStorageTest {
 
     @Test(expected = ExistStorageException.class)
     public void saveExist() throws Exception {
-        storage.save(storage.get(UUID_1));
+        storage.save(RESUME_1);
     }
 
     @Test(expected = StorageException.class)
-    public void overflow() throws Exception {
+    public void saveOverflow() throws Exception {
         Field f = storage.getClass().getSuperclass().getDeclaredField("STORAGE_LIMIT");
         f.setAccessible(true);
         int storageSize = (int) f.get(storage);
@@ -92,7 +103,7 @@ public class AbstractArrayStorageTest {
             for (int i = 0; i < storageSize; i++) {
                 storage.save(new Resume());
             }
-        } catch (StorageException e){
+        } catch (StorageException e) {
             fail("Failed when filled the storage");
         }
         storage.save(new Resume());
